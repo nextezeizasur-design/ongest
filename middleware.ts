dd27a30ca2d8003fe00bd6b6b0e451dc55c4b5b9
nextezeizasur-design <nextezeizasur@gmail.com>
2026-06-23 21:21:30 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -57,5 +57,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|api/auth).*)'],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|icons|manifest.json|api/auth).*)'],
 }
